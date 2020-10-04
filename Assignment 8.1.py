e83c68baf2fdf7218e4fdce261b5ca4c1f630e78
JDonilon <71718073+JDonilon@users.noreply.github.com>
2020-10-04 19:50:18 -0400

    WOrking

--- Assignment 8.1.py
+++ Assignment 8.1.py
@@ -4,8 +4,6 @@ import os, os.path
 
 #Prompt user for name of directory
 
-print(os.getcwd())
-
 desired_dir = input("Enter directory name: ")
 
 #Directory located
@@ -23,28 +21,57 @@ else:
 #Ask for file name
 filename = input("Enter a .txt file name: ")
 
+
+def verify():
+    print("[1] Verify Contact")
+    print("[2] Enter information again")
+
+    return int(input("Is this inforamtion correct? "))
+
+
 #Gather contact information
 def info():
 
     name = input("Enter your full name: ")
     address = input("Enter your street address: ")
     number = input("Enter your phone number: ")
+    print(name + ',' + address + ',' + number)
+    verify()
 
-    with open(filename, 'a') as file_object:
-        file_object.write("\n" + name + address + number)
+    if verify == 1:
+        with open(filename, 'a') as file_object:
+            file_object.write("\n" + name + ',' + address + ',' + number)
+            menu()
+    elif verify == 2:
+        info()
+    else:
+        print("That is not an option")
+        verify()
+
+
+#User chooses option
+def menu():
+    print("Welcome to your Contact List")
+    print("[1] Add Contact")
+    print("[2] Print Contact List")
+
+    return int(input("Please select a number to continue: "))
 
 
 def main():
 
     while True:
-        menu = input("Would you like to add a contact? Yes or No ")
-        if menu == "Yes":
+        menu_option = menu()
+        if menu_option == 1:
             info()
 
-        elif menu == "No":
+        elif menu_option == 2:
             f = open(filename, 'r')
             file_contents = f.read()
             print("Thank you for using the contact directory. Here are your contacts: \n" + file_contents)
             break
+        else:
+            print("Please select a menu option")
+
 
 main()
