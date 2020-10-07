b04c12503cd1b8da6e7bf8651ab96bd3f1006b73
JDonilon <71718073+JDonilon@users.noreply.github.com>
2020-10-07 09:44:18 -0400

    Add files via upload

---
+++ Assignment 8.1_Update.py
@@ -0,0 +1,80 @@
+# Project to create a directory to store contact information
+
+import os, os.path
+
+#Prompt user for name of directory
+
+desired_dir = input("Enter current directory or new directory: ")
+
+#Directory located
+if os.path.isdir(desired_dir):
+    print("Directory located")
+    os.chdir(desired_dir)
+    print("Your current directory is " + os.getcwd())
+
+#Directory not found
+else:
+    print("Directory Created")
+    os.mkdir(desired_dir)
+    os.chdir(desired_dir)
+    print("Your current directory is " + os.getcwd())
+
+#Ask for file name
+filename = input("Enter a .txt file name: ")
+
+
+def verify():
+    print("[1] Verify Contact")
+    print("[2] Enter information again")
+
+    return int(input("Is this inforamtion correct? "))
+
+
+#Gather contact information
+def info():
+
+    name = input("Enter your full name: ")
+    address = input("Enter your street address: ")
+    number = input("Enter your phone number: ")
+    print(name + ',' + address + ',' + number)
+    answer = verify()
+
+    while (True):
+      if answer == 1:
+        with open(filename, 'a') as file_object:
+          file_object.write("\n" + name + ',' + address + ',' + number)
+          menu()
+        break
+      elif answer == 2:
+        info()
+        break
+      else:
+        print("That is not an option")
+        verify()
+
+
+#User chooses option
+def menu():
+    print("Welcome to your Contact List")
+    print("[1] Add Contact")
+    print("[2] Print Contact List")
+
+    return int(input("Please select a number to continue: "))
+
+
+def main():
+    while True:
+        menu_option = menu()
+        if menu_option == 1:
+            info()
+
+        elif menu_option == 2:
+            f = open(filename, 'r')
+            file_contents = f.read()
+        print("Thank you for using the contact directory. Here are your contacts: \n" + file_contents)
+        break
+    else:
+        print("Please select a menu option")
+
+
+main()
